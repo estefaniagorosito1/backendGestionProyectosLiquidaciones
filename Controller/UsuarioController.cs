32f4f99fc59761ff37bdf87637b4c672a11acff2
Estefanía <estefania.gorosito.16@gmail.com>
2021-01-19 18:09:15 -0300

    ABM usuarios

---
+++ Controller/UsuarioController.cs
@@ -0,0 +1,62 @@
+﻿using BackendGestionProyectosLiquidaciones.Model;
+using BackendGestionProyectosLiquidaciones.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BackendGestionProyectosLiquidaciones.Controller
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private UsuarioService _usuarioService;
+
+        public UsuarioController(UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult CrearUsuario([FromBody] Usuario usuario)
+        {
+            var respuesta = _usuarioService.CrearUsuario(usuario);
+
+            if(!respuesta)
+            {
+                return BadRequest("Error al crear el usuario");
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Authorize]
+        public IActionResult ModificarUsuario([FromBody] Usuario usuario)
+        {
+            var respuesta = _usuarioService.ModificarUsuario(usuario);
+
+            if(!respuesta)
+            {
+                return BadRequest("Error al modificar el usuario.");
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{IdUsuario}")]
+        [Authorize]
+        public IActionResult EliminarUsuario(int IdUsuario)
+        {
+            _usuarioService.EliminarUsuario(IdUsuario);
+            return Ok();
+
+        }
+    }
+}
---
+++ Service/UsuarioService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendGestionProyectosLiquidaciones.Model;
+using BackendGestionProyectosLiquidaciones.Dao;
+
+namespace BackendGestionProyectosLiquidaciones.Service
+{
+    public class UsuarioService
+    {
+        private UsuarioDao _usuarioDao;
+
+        public UsuarioService(UsuarioDao usuarioDao)
+        {
+            _usuarioDao = usuarioDao;
+        }
+
+        public Usuario FindUsuario(Usuario usuario)
+        {
+            return _usuarioDao.FindUsuario(usuario);
+        }
+
+        /* ----- ABM ----- */
+        public bool CrearUsuario(Usuario usuario)
+        {
+            return _usuarioDao.CrearUsuario(usuario);
+        }
+
+        public bool ModificarUsuario(Usuario usuario)
+        {
+            return _usuarioDao.ModificarUsuario(usuario);
+        }
+
+        public void EliminarUsuario(int IdUsuario)
+        {
+            _usuarioDao.EliminarUsuario(IdUsuario);
+        }
+    }
+}
