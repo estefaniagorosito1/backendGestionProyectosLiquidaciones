e003c7dcaefb8262a7117cd575b692e1ae6990e0
Estefanía <estefania.gorosito.16@gmail.com>
2021-01-21 19:47:21 -0300

    Agrego stack de localidades

---
+++ Controller/LocalidadController.cs
@@ -0,0 +1,33 @@
+﻿using BackendGestionProyectosLiquidaciones.Model;
+using BackendGestionProyectosLiquidaciones.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendGestionProyectosLiquidaciones.Controller
+{
+    [Authorize]
+    [ApiController]
+    [Route("controller")]
+    public class LocalidadController : ControllerBase
+    {
+        private LocalidadService _localidadService;
+
+        public LocalidadController(LocalidadService localidadService)
+        {
+            _localidadService = localidadService;
+        }
+
+        [HttpGet("{IdProvincia}/{param}")]
+        [Authorize]
+        public IActionResult GetLocalidades([FromRoute] int IdProvincia, [FromRoute] string param)
+        {
+            List<Localidad> localidades = _localidadService.GetLocalidades(IdProvincia, param);
+            return Ok(localidades);
+        }
+
+    }
+}
---
+++ Service/LocalidadService.cs
@@ -0,0 +1,29 @@
+﻿using BackendGestionProyectosLiquidaciones.Dao;
+using BackendGestionProyectosLiquidaciones.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendGestionProyectosLiquidaciones.Service
+{
+    public interface ILocalidadService
+    {
+        List<Localidad> GetLocalidades(int IdProvincia, string param);
+    }
+
+    public class LocalidadService : ILocalidadService
+    {
+        private LocalidadDao _localidadDao;
+
+        public LocalidadService(LocalidadDao localidadDao)
+        {
+            _localidadDao = localidadDao;
+        }
+
+        public List<Localidad> GetLocalidades(int IdProvincia, string param)
+        {
+            return _localidadDao.GetLocalidades(IdProvincia, param);
+        }
+    }
+}
