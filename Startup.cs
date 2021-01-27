3853bcdeda477aa556ea9ed5ca18a232f6208e7b
Estefanía <estefania.gorosito.16@gmail.com>
2021-01-26 21:00:13 -0300

    Sigo agregando cosas para el login. Agrego parte para generar token

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using BackendGestionProyectosLiquidaciones.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using BackendGestionProyectosLiquidaciones.Service;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
 
 namespace BackendGestionProyectosLiquidaciones
 {
@@ -31,10 +37,52 @@ namespace BackendGestionProyectosLiquidaciones
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // JWT Authentication
+            var appSettings = Configuration.GetSection("AppSettings").Get<Settings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(auth =>
+            {
+                auth.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = ctx =>
+                    {
+                        var usuarioService = ctx.HttpContext.RequestServices.GetRequiredService<IUsuarioService>();
+                        var userId = int.Parse(ctx.Principal.Identity.Name);
+                        var user = usuarioService.FindUsuarioById(userId);
+                        if (user == null)
+                        {
+                            // return unauthorized if user no longer exists
+                            ctx.Fail("Unauthorized");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+
+                auth.RequireHttpsMetadata = false;
+                auth.SaveToken = true;
+                auth.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            // services.AddAuthorization();
+
         }
 
+        // -------------------------------------------------------------------------------------------------
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -48,11 +96,26 @@ namespace BackendGestionProyectosLiquidaciones
                 app.UseHsts();
             }
 
+            //global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseAuthentication();
+
+            // Con esto genero las url de los endpoints
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
         }
     }
 }
