c586162a5367fa7eac8f0acb3eff9b84a07dc7b4
Estefanía <estefania.gorosito.16@gmail.com>
2021-02-09 17:09:21 -0300

    Agrego endpoint para empleadoProyecto

---
+++ Controller/EmpleadoProyectoController.cs
@@ -0,0 +1,32 @@
+﻿using BackendGestionProyectosLiquidaciones.Model;
+using BackendGestionProyectosLiquidaciones.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendGestionProyectosLiquidaciones.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmpleadoProyectoController : ControllerBase
+    {
+        private IEmpleadoProyectoService _empleadoProyectoService;
+
+        public EmpleadoProyectoController(IEmpleadoProyectoService empleadoProyectoService)
+        {
+            _empleadoProyectoService = empleadoProyectoService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult AsignarEmpleadosProyecto([FromBody] EmpleadoProyecto empleadoProyecto)
+        {
+            _empleadoProyectoService.AsignarEmpleadosProyecto(empleadoProyecto);
+
+            return Ok("Empleado asignado al proyecto " + empleadoProyecto.IdproyectoNavigation.Descripcion);
+        }
+    }
+}
---
+++ Service/EmpleadoProyectoService.cs
@@ -0,0 +1,36 @@
+﻿using BackendGestionProyectosLiquidaciones.Model;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendGestionProyectosLiquidaciones.Service
+{
+    public interface IEmpleadoProyectoService
+    {
+        void AsignarEmpleadosProyecto(EmpleadoProyecto empleadoProyecto);
+    }
+
+
+    public class EmpleadoProyectoService : IEmpleadoProyectoService
+    {
+        private IServiceScopeFactory _scopeFactory;
+
+        public EmpleadoProyectoService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public void AsignarEmpleadosProyecto(EmpleadoProyecto empleadoProyecto)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<TpSeminarioContext>();
+
+                dbContext.EmpleadoProyecto.Add(empleadoProyecto);
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
