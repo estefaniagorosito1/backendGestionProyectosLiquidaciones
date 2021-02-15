c604e606fd12a51fbea396cba3b315405f9af1e6
EtcGonza <xetchegaraygonzalo@gmail.com>
2021-02-15 16:49:01 -0300

    Pusheo controladora y service de rol.

---
+++ Controller/RolController.cs
@@ -0,0 +1,33 @@
+﻿using BackendGestionProyectosLiquidaciones.Model;
+using BackendGestionProyectosLiquidaciones.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendGestionProyectosLiquidaciones.Controller
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    
+    public class RolController: ControllerBase
+    {
+        private IRolService _RolService;
+
+        public RolController(IRolService RolService)
+        {
+            _RolService = RolService;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetRoles()
+        {
+            var roles = _RolService.GetRoles();
+            return Ok(roles);
+        }
+    }
+}
---
+++ Service/RolService.cs
@@ -0,0 +1,39 @@
+﻿using BackendGestionProyectosLiquidaciones.Model;
+using BackendGestionProyectosLiquidaciones.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendGestionProyectosLiquidaciones.Service
+{
+
+    public interface IRolService
+    {
+        List<Rol> GetRoles();
+
+    }
+
+    public class RolService : IRolService
+    {
+
+        public IServiceScopeFactory _scopeFactory;
+
+        public RolService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public List<Rol> GetRoles()
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<TpSeminarioContext>();
+                return dbContext.Rol.ToList();
+            }
+        }
+    }
+}
